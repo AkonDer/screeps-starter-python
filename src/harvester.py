f8dbe7664e5bba186bcad5fd4dbf6701b085ce9d
AkonDer <vjacheslav.leb@gmail.com>
2023-01-11 22:50:29 +0300

    Functions implemented Harvester and Upgrader

--- src/harvester.py
+++ src/harvester.py
@@ -1,4 +1,5 @@
 from defs import *
+from helper import Helper
 
 __pragma__('noalias', 'name')
 __pragma__('noalias', 'undefined')
@@ -14,16 +15,38 @@ class Harvester:
 
     @staticmethod
     def run(creep, source):
-        if creep.memory.filling is True and creep.store[RESOURCE_ENERGY] < creep.store.getCapacity():
-            print(creep.store[RESOURCE_ENERGY])
-            if creep.harvest(source) == ERR_NOT_IN_RANGE:
-                creep.moveTo(source)
-        else:
+
+        spawn = Helper.near_spawn()
+
+        # Проверка полный ли крип
+        if creep.store[RESOURCE_ENERGY] == creep.store.getCapacity():
             creep.memory.filling = True
 
-        if creep.memory.filling:
-            pass
-        else:
-            print(creep.store[RESOURCE_ENERGY])
-            if creep.harvest(source) == ERR_NOT_IN_RANGE:
-                creep.moveTo(source)
+        if spawn.store[RESOURCE_ENERGY] < spawn.store.getCapacity(RESOURCE_ENERGY):
+            creep.memory.role = "Harvester"
+        elif spawn.store[RESOURCE_ENERGY] == spawn.store.getCapacity(RESOURCE_ENERGY):
+            creep.memory.role = "Upgrade"
+
+        if creep.memory.role == "Upgrade":
+            if creep.memory.filling:
+                if creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE:
+                    creep.moveTo(creep.room.controller)
+                else:
+                    if creep.store[RESOURCE_ENERGY] == 0:
+                        creep.memory.filling = False
+            else:
+                if creep.harvest(source) == ERR_NOT_IN_RANGE:
+                    creep.moveTo(source)
+
+        if creep.memory.role == "Harvester":
+            if creep.memory.filling:
+                if creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE:
+                    creep.moveTo(spawn)
+                else:
+                    if spawn.store[RESOURCE_ENERGY] == spawn.store.getCapacity(RESOURCE_ENERGY) \
+                            or creep.store[RESOURCE_ENERGY] == 0:
+                        creep.memory.filling = False
+            else:
+                if creep.harvest(source) == ERR_NOT_IN_RANGE:
+                    creep.moveTo(source)
+
---
+++ src/helper.py
@@ -0,0 +1,18 @@
+from defs import *
+
+__pragma__('noalias', 'name')
+__pragma__('noalias', 'undefined')
+__pragma__('noalias', 'Infinity')
+__pragma__('noalias', 'keys')
+__pragma__('noalias', 'get')
+__pragma__('noalias', 'set')
+__pragma__('noalias', 'type')
+__pragma__('noalias', 'update')
+
+
+class Helper:
+
+    @staticmethod
+    def near_spawn():
+        near_spawn = Object.keys(Game.spawns)[0]
+        return Game.spawns[near_spawn]
