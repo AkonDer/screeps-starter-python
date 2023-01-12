cfe091b891fa590669c0b69562ad1e145557787a
Ivan <van.koshkin.fr@gmail.com>
2023-01-12 14:32:46 +0300

    little refactoring

---
+++ src/creep.py
@@ -0,0 +1,23 @@
+from defs import *
+from helper import Helper
+
+__pragma__('noalias', 'name')
+__pragma__('noalias', 'undefined')
+__pragma__('noalias', 'Infinity')
+__pragma__('noalias', 'keys')
+__pragma__('noalias', 'get')
+__pragma__('noalias', 'set')
+__pragma__('noalias', 'type')
+__pragma__('noalias', 'update')
+
+
+class Creep:
+
+    def __init__(self, creep, source):
+        self.source = source
+        self.creep = creep
+        self.spawn = Helper.near_spawn()
+
+    def harvest_energy(self):
+        if self.creep.harvest(self.source) == ERR_NOT_IN_RANGE:
+            self.creep.moveTo(self.source)
