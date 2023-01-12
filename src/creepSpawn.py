9c1b3a8f38643f2113b672a44fe8bad241407999
Ivan <van.koshkin.fr@gmail.com>
2023-01-12 17:32:33 +0300

    added spawn builders

--- src/creepSpawn.py
+++ src/creepSpawn.py
@@ -16,16 +16,23 @@ class CreepSpawn:
     def __init__(self):
         self.spawn = Helper.near_spawn()
         self.creep_harvester_max = 3
+        self.creep_builder_max = 2
 
     def run(self):
-        if self.creep_count() < self.creep_harvester_max:
+        if self.creep_count()["Harvesters"] < self.creep_harvester_max:
             self.spawn.createCreep([WORK, WORK, CARRY, MOVE], None, {"role": "Harvester", "filling": False})
+        if self.creep_count()["Builders"] < self.creep_harvester_max:
+            self.spawn.createCreep([WORK, WORK, CARRY, MOVE], None, {"role": "Builder", "filling": False})
 
     @staticmethod
     def creep_count():
         num_harvesters = 0
+        num_builders = 0
         for name in Object.keys(Game.creeps):
             creep = Game.creeps[name]
             if creep.memory.role == "Harvester":
                 num_harvesters += 1
-        return num_harvesters
+            if creep.memory.role == "Builder":
+                num_builders += 1
+        return {"Harvesters": num_harvesters,
+                "Builders": num_builders}
